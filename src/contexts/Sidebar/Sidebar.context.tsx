6f020fdde640db7eaaf23fb11b10f0c521b64c55
alideweb <alimoradi0business@gmail.com>
2024-10-10 05:48:51 -0700

    fix: unused params bug fix

--- src/contexts/Sidebar/Sidebar.context.tsx
+++ src/contexts/Sidebar/Sidebar.context.tsx
@@ -2,22 +2,36 @@
 
 import React from 'react';
 
-export const SidebarContext = React.createContext({
-  isSidebarOpen: true,
-  setIsSidebarOpen: (
-    status: boolean | ((prevState: boolean) => boolean),
-  ): void => {},
-});
+export const SidebarContext =
+  React.createContext({
+    isSidebarOpen: true,
+    setIsSidebarOpen: (
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      _:
+        | boolean
+        | ((
+            prevState: boolean,
+          ) => boolean),
+    ): void => {},
+  });
 
 const SidebarContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState<boolean>(true);
+  const [
+    isSidebarOpen,
+    setIsSidebarOpen,
+  ] = React.useState<boolean>(true);
 
   return (
-    <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen }}>
+    <SidebarContext.Provider
+      value={{
+        isSidebarOpen,
+        setIsSidebarOpen,
+      }}
+    >
       {children}
     </SidebarContext.Provider>
   );
